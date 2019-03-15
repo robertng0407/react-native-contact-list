d04bceaacf6c62a7555b99ce3b71d67a733f3a9d
robertng0407 <robertng0407@gmail.com>
2019-03-15 12:24:01 -0700

    Add rendering of contacts list item

--- src/screens/Contacts.js
+++ src/screens/Contacts.js
@@ -11,6 +11,8 @@ import ContactListItem from '../components/ContactListItem';
 
 import { fetchContacts } from '../utils/api';
 
+const keyExtractor = ({ phone }) => phone;
+
 export default class Contacts extends Component {
     state = {
         contacts: [],
@@ -33,5 +35,48 @@ export default class Contacts extends Component {
                 error: true
             })
         }
+    };
+
+    renderContact = ({ item }) => {
+        const { name, avatar, phone } = item;
+
+        return (
+            <ContactListItem
+                name={name}
+                avatar={avatar}
+                phone={phone}
+            />
+        );
+    };
+
+    render() {
+        const { loading, contacts, error } = this.state;
+
+        const contactsSorted = contacts.sort((a, b) => (
+            a.name.localeCompare(b.name)
+        ));
+
+        return (
+            <View style={styles.container}>
+                {loading && <ActivityIndicator size="large" />}
+                {error && <Text>Error...</Text>}
+                {!loading &&
+                    !error && (
+                        <FlatList
+                            data={contactsSorted}
+                            keyExtractor={keyExtractor}
+                            renderItem={this.renderContact}
+                        />
+                    )}
+            </View>
+        );
     }
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        flex: 1
+    }
+});
