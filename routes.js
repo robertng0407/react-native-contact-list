f693c42de1cfa42ddd5e584315b475127f7fd743
robertng0407 <robertng0407@gmail.com>
2019-03-17 19:08:33 -0700

    Add favorites screen rendering contact thumbnails

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 import Contacts from './src/screens/Contacts';
 import Profile from './src/screens/Profile';
+import Favorites from './src/screens/Favorites';
 
 const AppNavigator = createStackNavigator({
     Contacts: {
@@ -9,9 +10,12 @@ const AppNavigator = createStackNavigator({
     },
     Profile: {
         screen: Profile
+    },
+    Favorites: {
+        screen: Favorites
     }
 }, {
-    initialRouteName: 'Contacts'
+    initialRouteName: 'Favorites'
 });
 
 export default createAppContainer(AppNavigator);
